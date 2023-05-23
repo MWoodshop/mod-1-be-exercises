e721400db9234732e725a2eded180a516e47e970
Mike Wood <mikewwood@icloud.com>
2023-05-23 16:53:42 -0400

    Test: add initialize test for Pet class with data.

--- practice_independent_challenges/week3/pets_and_customers/groomer_practice/spec/pet_spec.rb
+++ practice_independent_challenges/week3/pets_and_customers/groomer_practice/spec/pet_spec.rb
@@ -6,5 +6,12 @@ RSpec.describe Customer do
 
       expect(samson).to be_an_instance_of(Pet)
     end
+    it 'returns correct initialize data on Pet class' do
+      samson = Pet.new({ name: 'Samson', type: :dog, age: 3 })
+
+      expect(samson.name).to eq('Samson')
+      expect(samson.type).to eq(:dog)
+      expect(samson.age).to eq(3)
+    end
   end
 end
